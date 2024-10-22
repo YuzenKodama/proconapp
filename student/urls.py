f722454828fb499dfb6f9a89be5a3544e565e022
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-10-22 11:06:12 +0900

    生徒用側だけ完成

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,10 @@
 # login.urls.py
 from django.urls import path
-from .views import student_home,student_reason
+from .views import student_home,student_reason,student_changepass,student_logout
 
 urlpatterns = [
     path('', student_home, name='index'),
     path('reason/', student_reason, name='reason'),
+    path('changepass/', student_changepass, name='changepass'),
+    path('logout/', student_logout, name='logout'),
 ]
