5eb263e2cf6496664484a7eed61ce5aff04ae6e3
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-10-22 10:34:41 +0900

    理由画面途中

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,8 @@
 # login.urls.py
 from django.urls import path
-from .views import student_home
+from .views import student_home,student_reason
 
 urlpatterns = [
     path('', student_home, name='index'),
+    path('reason/', student_reason, name='reason'),
 ]
--- student/views.py
+++ student/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def student_home(request):
     return render(request, 'student/index.html')
+
+def student_reason(request):
+    return render(request, 'student/reason.html')
