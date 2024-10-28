b79369dd5248b669f9fb420794316ee5d7dade10
nanamalu0722 <fko2347096@stu.o-hara.ac.jp>
2024-10-28 11:29:27 +0900

    teacher

--- teacher/views.py
+++ teacher/views.py
@@ -1,20 +1,20 @@
 # student/views.py
 from django.shortcuts import render
 
-def student_changepass(request):
+def teacher_changepass(request):
     return render(request, 'teacher/base.html')
 
-def student_home(request):
+def teacher_home(request):
     return render(request, 'teacher/index.html')
 
-def student_reason(request):
+def teacher_reason(request):
     return render(request, 'teacher/changepass.html')
 
-def student_changepass(request):
+def teacher_changepass(request):
     return render(request, 'teacher/edit.html')
 
-def student_changepass(request):
+def teacher_changepass(request):
     return render(request, 'teacher/holiday.html')
 
-def student_logout(request):
+def teacher_logout(request):
     return render(request, 'teacher/logout.html')
