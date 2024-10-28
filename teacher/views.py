1471402d01b665e562932f7d2b223dc26ddf1700
nanamalu0722 <fko2347096@stu.o-hara.ac.jp>
2024-10-28 10:14:31 +0900

    teacher

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,20 @@
+# student/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def student_changepass(request):
+    return render(request, 'eacher/base.html')
+
+def student_home(request):
+    return render(request, 'teacher/index.html')
+
+def student_reason(request):
+    return render(request, 'eacher/changepass.html')
+
+def student_changepass(request):
+    return render(request, 'eacher/edit.html')
+
+def student_changepass(request):
+    return render(request, 'eacher/holiday.html')
+
+def student_logout(request):
+    return render(request, 'eacher/logout.html')
