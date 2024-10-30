e3fe3acadc910cbdaa765c4b2e0a1b57e3d23f16
nanamalu0722 <fko2347096@stu.o-hara.ac.jp>
2024-10-30 11:53:38 +0900

    sa

---
+++ admin/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from .views import admin_home,admin_base,admin_edit,admin_changepass,admin_logout,admin_index
+
+urlpatterns = [
+
+    path('', admin_home, name='index'),
+    path('base/', admin_base, name='base'),
+    path('edit/', admin_edit, name='edit'),
+    path('holiday/', admin_changepass, name='changepass'),
+    path('logout/', admin_logout, name='logout'),
+    path('logout/',  admin_index, name=' admin_situation_confirmation'),
+   
+]
--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,21 @@
+# student/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def admin_base(request):
+    return render(request, 'admin/base.html')
+
+def admin_home(request):
+    return render(request, 'admin/index.html')
+
+def admin_changepass(request):
+    return render(request, 'admin/changepass.html')
+
+def admin_edit(request):
+    return render(request, 'admin/situation_edit.html')
+
+def admin_holiday(request):
+    return render(request, 'admin/index.html')
+
+def admin_logout(request):
+    return render(request, 'admin/logout.html')
+
