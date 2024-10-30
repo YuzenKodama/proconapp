25c3a901c4a5ec897c4d2751b4853dbc41000614
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-10-30 10:12:11 +0900

    ログイン機能仮作成

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,8 @@
 # login.urls.py
 from django.urls import path
-from .views import login_home
+from .views import login_home, change_pass
 
 urlpatterns = [
-    path('', login_home, name='index'),
+    path('', login_home, name='login'),
+    path('password/', change_pass , name='change_pass'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,33 @@
 # login/views.py
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import CustomUser
 
 def login_home(request):
-    return render(request, 'index.html')
+    return render(request, 'login/login.html')
+
+def change_pass(request):
+    return render(request, 'login/Password.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        student_id = request.POST.get('student_id')
+        password = request.POST.get('password')
+        
+        # ユーザー認証
+        user = authenticate(request, username=student_id, password=password)
+        if user is not None:
+            login(request, user)
+
+            # 役割に応じたリダイレクト
+            if user.role == CustomUser.Role.STUDENT:
+                return redirect('student_view')  # 生徒のビューにリダイレクト
+            elif user.role == CustomUser.Role.TEACHER:
+                return redirect('teacher_view')  # 先生のビューにリダイレクト
+            elif user.role == CustomUser.Role.ADMIN:
+                return redirect('admin_view')  # 管理者のビューにリダイレクト
+        else:
+            messages.error(request, "学生番号またはパスワードが無効です。")
+    
+    return render(request, 'login.html')  # GETリクエストの場合の処理
