cedd84ae8d66801687635eedda971cb9bcb68c3b
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-11-05 10:55:15 +0900

    pasuwa-dohennkou

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,11 @@
 # login.urls.py
 from django.urls import path
-from .views import login_home, change_pass,login_view
+from .views import login_home, change_pass,login_view,CustomPasswordChangeView,PasswordChangeDoneView
 from student import views
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('password/', change_pass , name='change_pass'),
+    path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),  # パスワード変更ページ
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),  # 完了ページ
 ]
