8355ce842709fab1102d89267632898facba0a5e
yuzen <fko2347089@stu.o-hara.ac.jp>
2024-11-05 15:04:04 +0900

    modelhennkou

--- login/models.py
+++ login/models.py
@@ -4,15 +4,23 @@ from django.db import models
 class CustomUser(AbstractUser):
     # 新しいフィールドを追加
     student_id = models.CharField(max_length=32, unique=True)
+    
     class Role(models.IntegerChoices):
         STUDENT = 0
         TEACHER = 1
         ADMIN = 2
-    first_name = None
-    last_name = None
-    date_joined = None
-    groups = None
-    last_login = None
-    user_permissions = None
+    # user_permissionsフィールドの修正
+    user_permissions = models.ManyToManyField(
+        'auth.Permission', 
+        blank=True, 
+        related_name="customuser_permissions"
+    )
+    
+    # 新しいロールフィールド
     role = models.PositiveIntegerField(choices=Role.choices, default=Role.STUDENT)
-    is_active = models.BooleanField(default=True)  # 在籍フラグ
+    
+    # 在籍フラグ
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username  # 例: ユーザー名を返す
