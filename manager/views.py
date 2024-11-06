739e5882a0e65ca26bec24f648b904eae5ca4001
nanamalu0722 <fko2347096@stu.o-hara.ac.jp>
2024-11-06 15:06:17 +0900

    q

--- manager/views.py
+++ manager/views.py
@@ -5,7 +5,13 @@ def manager_index(request):
     return render(request, 'manager/index.html')
 
 def manager_log(request):
-    return render(request, 'manager/index.html')
+    return render(request, 'manager/log.html')
 
-def student_logout(request):
-    return render(request, 'student/logout.html')
+def manager_situation_edit(request):
+    return render(request, 'manager/lsituation_edit.html')
+
+def manager_student_management(request):
+    return render(request, 'manager/student_management.html')
+
+def manager_teacher_management(request):
+    return render(request, 'manager/teacher_management.html')
