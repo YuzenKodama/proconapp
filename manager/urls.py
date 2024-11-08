4d32824b85d36d06927a9e4de18e1a58bef8a4ab
nanamalu0722 <fko2347096@stu.o-hara.ac.jp>
2024-11-08 09:44:20 +0900

    a

--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,7 @@ from .views import manager_index,manager_log,manager_situation_edit,manager_stud
 
 
 urlpatterns = [
-    path('', manager_index, name='student_home'),
+    path('', manager_index, name='manager_home'),
     path('log/', manager_log, name='student_log'),
     path('situation_edit/', manager_situation_edit, name='situation_edit'),
     path('student_management/', manager_student_management, name='student_management'),
